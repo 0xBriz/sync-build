5ed48447a44d6a09aec6b48576899aa314c2bb3d
0xBriz <chaindevone@gmail.com>
2023-05-05 06:10:31 -0400

    log exp math

--- contracts/solidity-utils/contracts/math/FixedPointLite.sol
+++ contracts/solidity-utils/contracts/math/FixedPointLite.sol
@@ -2,7 +2,10 @@
 pragma solidity ^0.7.0;
 
 import "@balancer-labs/v2-interfaces/contracts/solidity-utils/helpers/BalancerErrors.sol";
-import "@balancer-labs/v2-solidity-utils/contracts/math/LogExpMath.sol";
+
+// import "@balancer-labs/v2-solidity-utils/contracts/math/LogExpMath.sol";
+
+// import "./LogExpMathLite.sol";
 
 /* solhint-disable private-vars-leading-underscore */
 
@@ -16,6 +19,35 @@ library FixedPointLite {
 
     // Minimum base for the power function when the exponent is 'free' (larger than ONE).
     uint256 internal constant MIN_POW_BASE_FREE_EXPONENT = 0.7e18;
+
+    // =============== LogExpMath
+
+    // All arguments and return values are 18 decimal fixed point numbers.
+    int256 constant ONE_18 = 1e18;
+
+    // Internally, intermediate values are computed with higher precision as 20 decimal fixed point numbers, and in the
+    // case of ln36, 36 decimals.
+    int256 constant ONE_20 = 1e20;
+    int256 constant ONE_36 = 1e36;
+
+    // The domain of natural exponentiation is bound by the word size and number of decimals used.
+    //
+    // Because internally the result will be stored using 20 decimals, the largest possible result is
+    // (2^255 - 1) / 10^20, which makes the largest exponent ln((2^255 - 1) / 10^20) = 130.700829182905140221.
+    // The smallest possible result is 10^(-18), which makes largest negative argument
+    // ln(10^(-18)) = -41.446531673892822312.
+    // We use 130.0 and -41.0 to have some safety margin.
+    int256 constant MAX_NATURAL_EXPONENT = 130e18;
+    int256 constant MIN_NATURAL_EXPONENT = -41e18;
+
+    // Bounds for ln_36's argument. Both ln(0.9) and ln(1.1) can be represented with 36 decimal places in a fixed point
+    // 256 bit integer.
+    int256 constant LN_36_LOWER_BOUND = ONE_18 - 1e17;
+    int256 constant LN_36_UPPER_BOUND = ONE_18 + 1e17;
+
+    uint256 constant MILD_EXPONENT_BOUND = 2 ** 254 / uint256(ONE_20);
+
+    // ====================
 
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         // Fixed Point addition is the same as regular checked addition
@@ -107,7 +139,8 @@ library FixedPointLite {
             uint256 square = mulDown(x, x);
             return mulDown(square, square);
         } else {
-            uint256 raw = LogExpMath.pow(x, y);
+            // uint256 raw = LogExpMathLite.pow(x, y);
+            uint256 raw = 0;
             uint256 maxError = add(mulUp(raw, MAX_POW_RELATIVE_ERROR), 1);
 
             if (raw < maxError) {
@@ -133,7 +166,8 @@ library FixedPointLite {
             uint256 square = mulUp(x, x);
             return mulUp(square, square);
         } else {
-            uint256 raw = LogExpMath.pow(x, y);
+            // uint256 raw = LogExpMathLite.pow(x, y);
+            uint256 raw = 0;
             uint256 maxError = add(mulUp(raw, MAX_POW_RELATIVE_ERROR), 1);
 
             return add(raw, maxError);
@@ -155,5 +189,93 @@ library FixedPointLite {
         // assembly {
         //     result := mul(lt(x, ONE), sub(ONE, x))
         // }
+    }
+
+    // ============== LogExpMath
+
+    /**
+     * @dev Exponentiation (x^y) with unsigned 18 decimal fixed point base and exponent.
+     *
+     * Reverts if ln(x) * y is smaller than `MIN_NATURAL_EXPONENT`, or larger than `MAX_NATURAL_EXPONENT`.
+     */
+    function pow(uint256 x, uint256 y) internal pure returns (uint256) {
+        if (y == 0) {
+            // We solve the 0^0 indetermination by making it equal one.
+            return uint256(ONE_18);
+        }
+
+        if (x == 0) {
+            return 0;
+        }
+
+        // Instead of computing x^y directly, we instead rely on the properties of logarithms and exponentiation to
+        // arrive at that result. In particular, exp(ln(x)) = x, and ln(x^y) = y * ln(x). This means
+        // x^y = exp(y * ln(x)).
+
+        // The ln function takes a signed value, so we need to make sure x fits in the signed 256 bit range.
+        _require(x >> 255 == 0, Errors.X_OUT_OF_BOUNDS);
+        int256 x_int256 = int256(x);
+
+        // We will compute y * ln(x) in a single step. Depending on the value of x, we can either use ln or ln_36. In
+        // both cases, we leave the division by ONE_18 (due to fixed point multiplication) to the end.
+
+        // This prevents y * ln(x) from overflowing, and at the same time guarantees y fits in the signed 256 bit range.
+        _require(y < MILD_EXPONENT_BOUND, Errors.Y_OUT_OF_BOUNDS);
+        int256 y_int256 = int256(y);
+
+        int256 logx_times_y;
+        if (LN_36_LOWER_BOUND < x_int256 && x_int256 < LN_36_UPPER_BOUND) {
+            int256 ln_36_x = _ln_36(x_int256);
+
+            // ln_36_x has 36 decimal places, so multiplying by y_int256 isn't as straightforward, since we can't just
+            // bring y_int256 to 36 decimal places, as it might overflow. Instead, we perform two 18 decimal
+            // multiplications and add the results: one with the first 18 decimals of ln_36_x, and one with the
+            // (downscaled) last 18 decimals.
+            logx_times_y = ((ln_36_x / ONE_18) * y_int256 + ((ln_36_x % ONE_18) * y_int256) / ONE_18);
+        } else {
+            logx_times_y = _ln(x_int256) * y_int256;
+        }
+        logx_times_y /= ONE_18;
+
+        // Finally, we compute exp(y * ln(x)) to arrive at x^y
+        _require(
+            MIN_NATURAL_EXPONENT <= logx_times_y && logx_times_y <= MAX_NATURAL_EXPONENT,
+            Errors.PRODUCT_OUT_OF_BOUNDS
+        );
+
+        // return uint256(exp(logx_times_y));
+
+        return 0;
+    }
+
+    function _ln(int256 a) internal pure returns (int256) {
+        if (a < ONE_18) {
+            // Since ln(a^k) = k * ln(a), we can compute ln(a) as ln(a) = ln((1/a)^(-1)) = - ln((1/a)). If a is less
+            // than one, 1/a will be greater than one, and this if statement will not be entered in the recursive call.
+            // Fixed point division requires multiplying by ONE_18.
+            return (-_ln((ONE_18 * ONE_18) / a));
+        }
+    }
+
+    /**
+     * @dev Intrnal high precision (36 decimal places) natural logarithm (ln(x))
+     * with signed 18 decimal fixed point argument, for x close to one.
+     *
+     * Should only be used if x is between LN_36_LOWER_BOUND and LN_36_UPPER_BOUND.
+     */
+    function _ln_36(int256 x) private pure returns (int256) {
+        // Since ln(1) = 0, a value of x close to one will yield a very small result,
+        // which makes using 36 digits worthwhile.
+
+        // First, we transform x to a 36 digit fixed point value.
+        x *= ONE_18;
+
+        // We will use the following Taylor expansion, which converges very rapidly. Let z = (x - 1) / (x + 1).
+        // ln(x) = 2 * (z + z^3 / 3 + z^5 / 5 + z^7 / 7 + ... + z^(2 * n + 1) / (2 * n + 1))
+
+        // Recall that 36 digit fixed point division requires multiplying by ONE_36, and multiplication requires
+        // division by ONE_36.
+        int256 z = ((x - ONE_36) * ONE_36) / (x + ONE_36);
+        int256 z_squared = (z * z) / ONE_36;
     }
 }
